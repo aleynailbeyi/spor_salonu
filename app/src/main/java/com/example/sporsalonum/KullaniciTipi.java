050872d6d95a2f8a5226a994b80601990e3f3ca0
aleyna1401 <alynailbeyi@gmail.com>
2021-01-10 14:31:17 +0300

    kullanici tipi eklendi

---
+++ app/src/main/java/com/example/sporsalonum/KullaniciTipi.java
@@ -0,0 +1,23 @@
+package com.example.sporsalonum;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class KullaniciTipi extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kullanici_tipi, container, false);
+    }
+}
