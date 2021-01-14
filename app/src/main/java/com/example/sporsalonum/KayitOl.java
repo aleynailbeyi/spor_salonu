4314091144a27ab035756cb2f920c35a5fb389b9
aleyna1401 <alynailbeyi@gmail.com>
2021-01-15 00:08:46 +0300

    kullanici tipi fragmentler eklendi

--- app/src/main/java/com/example/sporsalonum/KayitOl.java
+++ app/src/main/java/com/example/sporsalonum/KayitOl.java
@@ -2,13 +2,17 @@ package com.example.sporsalonum;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -23,11 +27,12 @@ import com.google.firebase.auth.FirebaseAuth;
 public class KayitOl extends AppCompatActivity {
 
     EditText email, username, parola;
-    RadioGroup rg;
+//    RadioGroup rg;
+//    RadioButton radioButton,radioButton2;
     Button kayitOl;
     ProgressBar progressBar;
     private FirebaseAuth mAuth;
-    String kullanici_tipi = "";
+//    String kullanici_tipi = "";
 
 
     @Override
@@ -36,39 +41,35 @@ public class KayitOl extends AppCompatActivity {
         setContentView(R.layout.activity_kayit_ol);
         mAuth=FirebaseAuth.getInstance();
         tanimla();
-//        btnKayit();
         btnTikla();
+
     }
+    public void fragmentGoster(){
+        KullaniciTipi kTipi = new KullaniciTipi();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.kayitOl, kTipi,"kullanici tipi");
+        transaction.commit();
+    }
+
 
     public void tanimla(){
        email = ((EditText) findViewById(R.id.et_register_email));
        username = ((EditText) findViewById(R.id.et_register_name));
        parola = ((EditText) findViewById(R.id.et_register_parola));
-       rg = ((RadioGroup) findViewById(R.id.rg_kayit));
+ //      rg = ((RadioGroup) findViewById(R.id.rg_kayit));
        kayitOl = ((Button) findViewById(R.id.btn_register_kayitol));
        progressBar = ((ProgressBar) findViewById(R.id.message));
-
     }
 
-//    public void btnKayit() {
-//        if (sporcu_rb.isSelected()) {
-//            Intent i = new Intent(KayitOl.this, SporcuProfile.class);
-//            startActivity(i);
-//            finish();
-//        } else if (salon_rb.isSelected()) {
-//            Intent i = new Intent(KayitOl.this, SalonProfile.class);
-//            startActivity(i);
-//            finish();
-//        } else {
-//            Toast.makeText(KayitOl.this, "HATA !", Toast.LENGTH_LONG).show();
-//        }
-//    }
     public void btnTikla(){
+
         kayitOl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String Email = email.getText().toString().trim();
                 String password = parola.getText().toString().trim();
+
 
                 if(TextUtils.isEmpty(Email)){
                     email.setError("Email alani bos birakilamaz...");
@@ -79,22 +80,22 @@ public class KayitOl extends AppCompatActivity {
                     return;
                 }
 
-                int radioButtonID = rg.getCheckedRadioButtonId();
-                View rb = rg.findViewById(radioButtonID);
-                int idx = rg.indexOfChild(rb);
-                System.out.println(idx);
-
-                if(idx == 0){
-                    kullanici_tipi = "Sporcu";
-                }
-                else if (idx == 1) {
-                    kullanici_tipi = "Salon";
-                }
-                else {
-                    Toast.makeText(KayitOl.this,"Hata! Kullanıcı tipi seçmediniz!" ,Toast.LENGTH_SHORT).show();
-                }
-                System.out.println(kullanici_tipi);
-                progressBar.setVisibility(View.VISIBLE);
+//                int radioButtonID = rg.getCheckedRadioButtonId();
+//                View rb = rg.findViewById(radioButtonID);
+//                int idx = rg.indexOfChild(rb);
+//                System.out.println(idx);
+//
+//                if(idx == 0){
+//                    kullanici_tipi = "Sporcu";
+//                }
+//                else if (idx == 1) {
+//                    kullanici_tipi = "Salon";
+//                }
+//                else {
+//                    Toast.makeText(KayitOl.this,"Hata! Kullanıcı tipi seçmediniz!" ,Toast.LENGTH_SHORT).show();
+//                }
+//                System.out.println(kullanici_tipi);
+                  progressBar.setVisibility(View.VISIBLE);
 
                 mAuth.createUserWithEmailAndPassword(Email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
@@ -102,7 +103,7 @@ public class KayitOl extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(KayitOl.this,"Kullanici Kaydedildi.",Toast.LENGTH_SHORT).show();
                             System.out.println(mAuth.getCurrentUser().getEmail());
-
+                            fragmentGoster();
                         }
                         else{
                             Toast.makeText(KayitOl.this,"HATA !" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/sporsalonum/KullaniciTipi.java
+++ app/src/main/java/com/example/sporsalonum/KullaniciTipi.java
@@ -3,21 +3,82 @@ package com.example.sporsalonum;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class KullaniciTipi extends Fragment {
 
+    RadioButton rbSporcu;
+    RadioButton rbSalon;
+    View view;
+  // String kullanici_tipi = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    public void gecSporcuFragment(View v){
+        SporcuProfile sporcuProfile = new SporcuProfile();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_kullanici_tipi,sporcuProfile,"Sporcu Profile Fragment");
+        transaction.commit();
+    }
+    public void gecSalonFragment(View v){
+        SalonProfile salonProfile = new SalonProfile();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_kullanici_tipi,salonProfile,"Salon Profile Fragment");
+        transaction.commit();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_kullanici_tipi, container, false);
+        View v = inflater.inflate(R.layout.fragment_kullanici_tipi, container, false);
+        onRadioButtonClicked(view);
+
+
+//                int radioButtonID = rg.getCheckedRadioButtonId();
+//                View rb = rg.findViewById(radioButtonID);
+//                int idx = rg.indexOfChild(rb);
+//                System.out.println(idx);
+//
+//                if(idx == 0){
+//                    kullanici_tipi = "Sporcu";
+//                    gecSporcuFragment(myView);
+//                }
+//                else if (idx == 1) {
+//                    kullanici_tipi = "Salon";
+//                    gecSalonFragment(myView);
+//                }
+//                System.out.println(kullanici_tipi);
+     return v;
     }
+    public void onRadioButtonClicked(View view){
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()){
+            case R.id.rb_sporcu:
+                if(checked){
+                    gecSporcuFragment(rbSporcu);
+                    break;
+                }
+            case R.id.rb_salon:
+                if(checked){
+                    gecSalonFragment(rbSalon);
+                    break;
+                }
+        }
+    }
+
 }
